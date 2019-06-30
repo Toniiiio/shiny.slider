7c1b70f852b370436b9bf27941ada20f474d74ff
Tonio Liebrand <andreas.liebrand@hof.uni-frankfurt.de>
2019-06-30 11:25:19 +0200

    refactoring

--- R/app.R
+++ R/app.R
@@ -2,9 +2,11 @@
 library(shiny)
 
 ui <- fluidPage(
+  
   tags$script(src = "jquery-1.6.1.min.js"),
   tags$script(src = "jquery-ui-1.8.13.custom.min.js"),
   tags$script(src = "jquery.beforeafter-1.4.min.js"),
+  
   tags$script("
             $(function() {
                 $('#slider').beforeAfter({
@@ -15,12 +17,25 @@ ui <- fluidPage(
                 })
               });
   "),
+  
   mainPanel(
-    tags$div(id = "slider",
-               img(src = "img/Polar-Bear-SliderTest1.jpg", width = 800, height = 533),
-               img(src = "img/Polar-Bear-SliderTest2.jpg", width = 800, height = 533)
+    
+    tags$div(
+      id = "slider",
+      img(
+        src = "img/Polar-Bear-SliderTest1.jpg", 
+        width = 800, 
+        height = 533
+      ),
+      
+      img(
+        src = "img/Polar-Bear-SliderTest2.jpg", 
+        width = 800, 
+        height = 533
+      )
     )
   )
+  
 )
 
 server <- function(input, output, session){}
